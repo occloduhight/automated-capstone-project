5dbae36667d24a001d4d705c66dba6ed3dc92059
occloduhight <chinweodochi@gmail.com>
2025-12-09 21:56:31 +0100

    update code

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "cidr" {}
+variable "pub_sn1" {}
+variable "pub_sn2" {}
+variable "prv_sn1" {}
+variable "prv_sn2" {}
+variable "redhat_ami" {}
+variable "instance_type" {}
+variable "db_name" {}
+variable "dbcred" {}
+variable "db_username" {}
+variable "db_password" {}
+variable "alert_email" {}
